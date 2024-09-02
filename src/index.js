e4dcbb10fc9e08f4e6f2bb51cc05e1b69845282b
Verso <vuoreol@pm.me>
2024-09-02 11:09:23 +0300

    added

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional: for any global styles
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Render the App component into the root div
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Optional: For measuring performance in your app
+reportWebVitals();
